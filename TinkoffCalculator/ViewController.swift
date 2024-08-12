2120d9c29018afb658c544594da4b0bb451bcc6d
londxz <rodionn39@gmail.com>
2024-08-12 17:11:44 +0300

    nicer ver without priority

--- TinkoffCalculator/ViewController.swift
+++ TinkoffCalculator/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 enum CalculationError: Error {
     case divisionByZero
 }
+
+
 enum Operation: String {
     case add = "+"
     case substract = "-"
     case multiply = "x"
     case divide = "/"
     
+    enum OperationPriority: Int {
+        case second = 0
+        case first
+    }
+    
+    func priority() -> Int {
+        switch self {
+        case .add, .substract: 
+            return OperationPriority.second.rawValue
+        case .divide, .multiply: 
+            return OperationPriority.first.rawValue
+        }
+    }
     func calculate(_ num1: Double, _ num2: Double) throws -> Double {
         switch self {
         case .add: 
@@ -45,12 +60,16 @@ class ViewController: UIViewController {
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         guard let buttonText = sender.currentTitle else { return }
-
+        
+        if flagOperationPressed == true {
+            resetLabelText()
+            flagOperationPressed = false
+        }
+        
         if buttonText == "," && label.text?.contains(",") == true {
             return
         }
-        
-        if label.text == "0" {
+        if label.text == "0" && buttonText != ","{
             label.text = buttonText
         } else {
             label.text?.append(buttonText)
@@ -59,26 +78,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func operationButtonPressed(_ sender: UIButton) {
-        guard 
-            let buttonText = sender.currentTitle,
-            let buttonOperation = Operation(rawValue: buttonText)
-        else { return }
-        
-        //print(buttonText)
-        //label.text = buttonText
-        
         guard
-            let labelText = label.text,
-            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue
+            let buttonText = sender.currentTitle, // присвоили строку кнопки
+            let buttonOperation = Operation(rawValue: buttonText) // присвоили операцию
         else { return }
-        
+        guard
+            let labelText = label.text, // из кнопки делаем строку-число
+            let labelNumber = numberFormatter.number(from: labelText)?.doubleValue // привели строку к числу
+        else { return }
         calculationHistoryItem.append(.number(labelNumber))
         calculationHistoryItem.append(.operation(buttonOperation))
-        resetLabelText()
+        //print("add \(labelNumber)")
+        //print("add \(buttonOperation)")
+        label.text = labelText
+        flagOperationPressed = true
     }
     
+    var flagOperationPressed = false
     var calculationHistoryItem = [CalculationHistoryItem]()
-    
     @IBAction func clearButtonPressed() {
         calculationHistoryItem.removeAll()
         resetLabelText()
@@ -91,6 +108,7 @@ class ViewController: UIViewController {
         else { return }
         
         calculationHistoryItem.append(.number(labelNumber))
+        //print("add \(labelNumber)")
         
         do {
             let result = try calculate()
@@ -116,6 +134,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         resetLabelText()
         // Do any additional setup after loading the view.
+        //let op = Operation.OperationPriority.first
+        //print(op.rawValue)
     }
     
     func resetLabelText() {
@@ -123,9 +143,9 @@ class ViewController: UIViewController {
     }
     
     func calculate() throws -> Double {
-        guard case .number(let firstNumber) = calculationHistoryItem[0] else { return 0 }
+        guard case .number(let firstNumber) = calculationHistoryItem[0] else { return 0 } // добавил первое число
         
-        var currentResult = firstNumber
+        var currentResult = firstNumber // первое число - текущий ответ
         
         for index in stride(from: 1, to: calculationHistoryItem.count - 1, by: 2) {
             guard
@@ -133,6 +153,7 @@ class ViewController: UIViewController {
                 case .number(let number) = calculationHistoryItem[index + 1]
             else { break }
             currentResult = try operation.calculate(currentResult, number)
+            //print("counted \(currentResult)")
         }
         return currentResult
     }
